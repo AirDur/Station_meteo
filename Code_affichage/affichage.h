2443a6a8cfa768f861325c2c73e4f5a65bf9c0d5
Ana-Maria <anamariatucaliuc@yahoo.com>
2018-04-19 11:24:06 +0200

    ==Modification 2 fonctions pour les boutons + affichage message écran station

---
+++ Code_affichage/affichage.h
@@ -0,0 +1,22 @@
+#ifndef _AFFICHAGE_H
+#define _AFFICHAGE_H
+
+
+#define MWINCLUDECOLORS
+#include <stdio.h>
+#include "nano-X.h"
+#include <string.h>
+
+#include<time.h>
+#include<signal.h>
+#include <graphics.h>
+
+
+#include <fcntl.h>
+#include <unistd.h>
+
+
+void affiche_message_ecran(int acgc,char **argv);
+
+
+#endif /* _AFFICHAGE_H*/
