2dec794396e487787747c218ee2a2d7ae033960b
Ana-Maria <anamariatucaliuc@yahoo.com>
2018-04-19 10:03:31 +0200

    Recuperation boutons+affichage

---
+++ Code_affichage/affichage.c
@@ -0,0 +1,17 @@
+/**
+ * @file     	affichage.c
+ * @author
+ * @brief    	Fontion de base permettant d'afficher sur l'écran un message
+ */
+
+#define MWINCLUDECOLORS
+#include <stdio.h>
+
+
+int main(int ac,char **av)
+{
+    printf("Hello world") ;
+
+    return 0;
+
+} // main
