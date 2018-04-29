5af7d99e7ae45a295ec3e3df1391700416002b24
tgattaz <thomas.gattaz@gmail.com>
2018-04-29 19:07:35 +0200

    FD

--- STATION_METEO_2018/capteurs.c
+++ STATION_METEO_2018/capteurs.c
@@ -12,6 +12,9 @@
 
 
 #include "capteurs.h"
+#include <graphics.h>
+#include <nano-X.h>
+#include <pthread.h>
 #define ADC_CHAN_PRESSURE    4
 #define ADC_CHAN_HUMIDITY    5
 
@@ -165,5 +168,44 @@ int main(int argc, char**argv)
   printf(" - Valeur de la pression : %lf \n", P);
   //Affichage humidité
   printf(" - Valeur de l'humidité : %lf \n", RH);
+
+  GR_WINDOW_ID w;
+  GR_GC_ID gc;
+  GR_EVENT event;
+
+  if (GrOpen() < 0)
+  {
+    printf("Can't open graphics\n");
+    exit(1);
+  }
+
+  // Configuration de l'ecran
+  w = GrNewWindow(1, 0, 0, 160, 240, 0, BLACK, WHITE);
+  gc = GrNewGC();
+  GrSetGCForeground(gc, WHITE);
+  GrSetGCUseBackground(gc, GR_FALSE);
+  GrSelectEvents(w, GR_EVENT_MASK_EXPOSURE);
+  GrMapWindow(w);
+
+  char sT[64], sP[64], sRH[64];
+  sprintf(sT, "%.1f °C\n",temperature);
+  sprintf(sP, "%.1f bar\n",P);
+  sprintf(sRH, "%.1f %%\n",RH);
+
+  GrFillRect(w,gc,0,0,160,4);
+  GrFillRect(w,gc,0,4,4,240);
+  GrFillRect(w,gc,0,236,160,4);
+  GrFillRect(w,gc,156,4,4,240);
+  GrFillRect(w,gc,0,40,160,2);
+  GrFillRect(w,gc,80,40,2,200);
+  GrFillRect(w,gc,80,140,80,2);
+  GrText(w, gc, 50, 30, "DURET le sang", -1, GR_TFASCII);
+
+  GrText(w, gc, 15, 145,  sT,  -1, GR_TFASCII);
+  GrText(w, gc, 90, 100, sP,  -1, GR_TFASCII);
+  GrText(w, gc, 90, 190, sRH, -1, GR_TFASCII);
+
+
+
   return EXIT_SUCCESS;
 }
