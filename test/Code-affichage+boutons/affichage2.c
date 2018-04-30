8d0016a2865fd8b45ca0e1daba9360dda01335fb
Ana-Maria <anamariatucaliuc@yahoo.com>
2018-04-30 11:24:51 +0200

    ==Test changement affichage+boutons

---
+++ test/Code-affichage+boutons/affichage2.c
@@ -0,0 +1,44 @@
+/**
+ * @file     	affichage.c
+ * @author
+ * @brief    	Fontion de base permettant d'afficher sur l'écran un message
+ */
+
+#include "affichage.h"
+
+int main(int acgc,char **argv)
+{
+  	GR_WINDOW_ID w;
+    GR_EVENT event;
+    GR_FONT_ID font;
+
+  	system("./nano-X &");
+
+
+  	//configuration pour les préférences d'afichage
+  	if (GrOpen() < 0)                            //fonction graphics routines
+  	{
+          	printf("Can't open graphics\n");
+          	exit(1);
+      	}
+
+        // parametrès dans l'ordre parent, coordonées x et y (20,100) ,
+        //largeur 120, hauteur 120 , bordersize 5 ,backgroud WHITE , bordercolor BLACK
+
+      	w = GrNewWindow(GR_ROOT_WINDOW_ID, 0, 0, 240, 240,5, WHITE, RED);
+        GrMapWindow(w);
+
+          //function allocates a new graphic context with all parameters set to the default values.
+          gc = GrNewGC();
+          font=GrCreateFont("Helvetica",6,NULL);
+          GrSetFontRotation(font,90);
+
+          GrText(w, gc, 0, 20,"Coucou" , 6, GR_TFASCII);
+          /*Enter event loop **/
+          for(;;)
+            GrGetNextEvent(&event);
+
+          //function flushes any buffered function calls and closes the connection created with the GrOpen function.
+         GrClose();
+         return (0);
+}
