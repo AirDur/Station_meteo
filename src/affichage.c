b7108c92fd4aa6c054feb15abb0e2bfd517f4c48
AirDur <ralphagris@gmail.com>
2018-04-30 10:32:43 +0200

    .

--- src/affichage.c
+++ src/affichage.c
@@ -2,47 +2,8 @@
 
 
 
-void affichage(double temperature, double pression, double humidite) {
-/*  GR_WINDOW_ID w;
-  GR_GC_ID gc;
-  GR_EVENT event;
-  char sT[64], sP[64], sRH[64];
+int affichage(double temperature, double pression, double humidite) {
 
-  if (GrOpen() < 0)
-  {
-    printf("Can't open graphics\n");
-    exit(1);
-  }
-
-  // Configuration de l'ecran
-  w = GrNewWindow(1, 0, 0, 160, 240, 0, BLACK, WHITE);
-  gc = GrNewGC();
-  GrSetGCForeground(gc, WHITE);
-  GrSetGCUseBackground(gc, GR_FALSE);
-  GrSelectEvents(w, GR_EVENT_MASK_EXPOSURE);
-  GrMapWindow(w);
-
-
-  sprintf(sT, "%.1f °C\n", temperature);
-  sprintf(sP, "%.1f bar\n", pression);
-  sprintf(sRH, "%.1f %%\n", humidite);
-
-  GrFillRect(w,gc,0,0,160,4);
-  GrFillRect(w,gc,0,4,4,240);
-  GrFillRect(w,gc,0,236,160,4);
-  GrFillRect(w,gc,156,4,4,240);
-  GrFillRect(w,gc,0,40,160,2);
-  GrFillRect(w,gc,80,40,2,200);
-  GrFillRect(w,gc,80,140,80,2);
-  GrText(w, gc, 50, 30, "DURET le sang", -1, GR_TFASCII);
-
-  GrText(w, gc, 15, 145,  sT,  -1, GR_TFASCII);
-  GrText(w, gc, 90, 100, sP,  -1, GR_TFASCII);
-  GrText(w, gc, 90, 190, sRH, -1, GR_TFASCII);
-
-  GrClose();
-}
-*/
 GR_WINDOW_ID w;
 GR_EVENT event;
 GR_GC_ID gc;
@@ -54,7 +15,7 @@ system("./nano-X &");
 if (GrOpen() < 0)                            //fonction graphics routines
 {
     printf("Can't open graphics\n");
-    exit(1);
+    return EXIT_FAILURE;
 }
 
 // parametrès dans l'ordre parent, coordonées x et y (20,100) ,
@@ -75,7 +36,41 @@ for(;;)
   //function flushes any buffered function calls and closes the connection created with the GrOpen function.
  GrClose();
 
+ /*
+   char sT[64], sP[64], sRH[64];
 
- //return EXIT_SUCCESS;
+   if (GrOpen() < 0)
+   {
+     printf("Can't open graphics\n");
+     exit(1);
+   }
 
+   // Configuration de l'ecran
+   w = GrNewWindow(1, 0, 0, 160, 240, 0, BLACK, WHITE);
+   gc = GrNewGC();
+   GrSetGCForeground(gc, WHITE);
+   GrSetGCUseBackground(gc, GR_FALSE);
+   GrSelectEvents(w, GR_EVENT_MASK_EXPOSURE);
+   GrMapWindow(w);
+
+
+   sprintf(sT, "%.1f °C\n", temperature);
+   sprintf(sP, "%.1f bar\n", pression);
+   sprintf(sRH, "%.1f %%\n", humidite);
+
+   GrFillRect(w,gc,0,0,160,4);
+   GrFillRect(w,gc,0,4,4,240);
+   GrFillRect(w,gc,0,236,160,4);
+   GrFillRect(w,gc,156,4,4,240);
+   GrFillRect(w,gc,0,40,160,2);
+   GrFillRect(w,gc,80,40,2,200);
+   GrFillRect(w,gc,80,140,80,2);
+   GrText(w, gc, 50, 30, "DURET le sang", -1, GR_TFASCII);
+
+   GrText(w, gc, 15, 145,  sT,  -1, GR_TFASCII);
+   GrText(w, gc, 90, 100, sP,  -1, GR_TFASCII);
+   GrText(w, gc, 90, 190, sRH, -1, GR_TFASCII);
+
+ */
+ return EXIT_SUCCESS;
 }
