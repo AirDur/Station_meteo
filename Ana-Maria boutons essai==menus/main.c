860399f0772718a8758b261cf2780d4d7087393b
AirDur <ralphagris@gmail.com>
2018-05-22 12:56:55 +0200

    main.c restructured

--- Ana-Maria boutons essai==menus/main.c
+++ Ana-Maria boutons essai==menus/main.c
@@ -8,64 +8,51 @@ int main(int argc, char**argv)
 
   double temperature = 0.0, humidite = 0.0, pression = 0.0;
 
-  int ok = 1;
-  do{
-       ok = 0;
-       while(ok == 0)
-        {
-            if(Push_Button() == 1)
-            {
-              printf("===Premier button appuyé===\n") ;
-               if ( affichage_main_bouton_1() != EXIT_SUCCESS )
-				  {
-					  perror("[main.c] affichage bouton 1");
-					  return EXIT_FAILURE;
-				  }
-				  
-              sleep(2);
-              while(Push_Button() != 4){}
-              ok = 1;
-              break;
-            }
-            if(Push_Button() == 2)
-            {
-              printf("===Deuxième bouton===\n") ;
-              if ( affichage_main_bouton_2() != EXIT_SUCCESS )
-				  {
-					  perror("[main.c] affichage bouton 2");
-					  return EXIT_FAILURE;
-				  }
-              sleep(2);
-              while(Push_Button() != 4){}
-              ok = 1;
-              break;
-            }
-            if(Push_Button() == 3)
-            {
-              printf("===Troisième bouton===\n") ;
-              if ( affichage_main_bouton_3() != EXIT_SUCCESS )
-				  {
-					  perror("[main.c] affichage bouton 3");
-					  return EXIT_FAILURE;
-				  }
-              sleep(4);
-              ok = 0;
-              break;
-            }
-            if(Push_Button() == 4)
-            {
-              printf("===Quatrième bouton===\n") ;
-              if ( affichage_main_bouton_4() != EXIT_SUCCESS )
-				  {
-					  perror("[main.c] affichage bouton 4");
-					  return EXIT_FAILURE;
-				  }
-              sleep(4);
-              ok = 0;
-              break;
-            }
-        }
-}while(ok == 1);
+  int execute = 1;
+
+  do {
+    unsigned int pressed = Push_Button();
+    if(pressed == 1) {
+      printf("===Premier button appuyé===\n") ;
+      if ( affichage_main_bouton_1() != EXIT_SUCCESS ) {
+			  perror("[main.c] affichage bouton 1");
+			  return EXIT_FAILURE;
+		  }
+		  sleep(2);
+      //while(Push_Button() != 4){}
+      //break;
+    }
+    else if(pressed == 2) {
+      printf("===deuxième button appuyé===\n") ;
+      if ( affichage_main_bouton_1() != EXIT_SUCCESS ) {
+			  perror("[main.c] affichage bouton 2");
+			  return EXIT_FAILURE;
+		  }
+		  sleep(2);
+      //while(Push_Button() != 4){}
+      break;
+    }
+    else if(pressed == 3) {
+      printf("===Troisième button appuyé===\n") ;
+      if ( affichage_main_bouton_1() != EXIT_SUCCESS ) {
+			  perror("[main.c] affichage bouton 3");
+			  return EXIT_FAILURE;
+		  }
+		  sleep(2);
+      //while(Push_Button() != 4){}
+      break;
+    } else if(pressed == 4) {
+      printf("===Quatrieme button appuyé===\n") ;
+      if ( affichage_main_bouton_1() != EXIT_SUCCESS ) {
+			  perror("[main.c] affichage bouton 4");
+			  return EXIT_FAILURE;
+		  }
+		  sleep(2);
+      //while(Push_Button() != 4){}
+      execute = 0;
+      break;
+    }
+  } while ( execute == 1 );
 
   return 0;
 }
